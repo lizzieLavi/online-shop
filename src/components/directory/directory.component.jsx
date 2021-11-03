efa5c60c54e8b510ca416a36c8f0242b03d97158
unknown <lizzie.olishuk1@gmail.com>
2021-11-03 16:01:01 +0200

    update menu-items to navigate with react-router-dom

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -51,8 +51,8 @@ class Directory extends React.Component {
     {
         return (
             <div className='directory_menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) =>
-                 <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size}  /> )}
+                {this.state.sections.map(({id,...OtherSectionProps}) =>
+                 <MenuItem key={id} {...OtherSectionProps} />)}
             </div>
         )
     }
--- src/components/menu_item/menu_item.component.jsx
+++ src/components/menu_item/menu_item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './menu_item.styles.scss'
+import { withRouter } from 'react-router';
 
-const MenuItem = ({title, imageUrl, size}) =>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) =>
 (
-   <div className={`${size} menu_item`}>
+   <div className={`${size} menu_item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
        <div className='background_image' 
         style={{backgroundImage: `url(${imageUrl})`}} > 
        </div>
@@ -16,4 +17,4 @@ const MenuItem = ({title, imageUrl, size}) =>
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
