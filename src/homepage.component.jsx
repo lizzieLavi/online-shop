636896897c2b53ae4be13cb5dec0a1bcd379d237
unknown <lizzie.olishuk1@gmail.com>
2021-11-03 10:00:53 +0200

    first-commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+const HomePage = () =>
+(
+    <div className='homepage'>
+        <div className='directory_menu'>
+            <div className='menu_item'>
+                <div className='content'>
+                    <h1 className='title'>
+                        Hats
+                    </h1>
+                    <span className='subTitle'> Shop</span>
+                </div>
+                <div className='content'>
+                    <h1 className='title'>
+                       Jackets
+                    </h1>
+                    <span className='subTitle'> Shop</span>
+                </div>
+                <div className='content'>
+                    <h1 className='title'>
+                        Sneakers
+                    </h1>
+                    <span className='subTitle'> Shop</span>
+                </div>
+                <div className='content'>
+                    <h1 className='title'>
+                        Womans
+                    </h1>
+                    <span className='subTitle'> Shop</span>
+                </div>
+                <div className='content'>
+                    <h1 className='title'>
+                        Mans
+                    </h1>
+                    <span className='subTitle'> Shop</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
+
